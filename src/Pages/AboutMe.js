fc1b2d17499a6835201a1da02bc60aba1a6d848d
Sumant Guha <sumant@Sumants-MacBook-Air.local>
2024-12-08 21:40:51 -0800

    Updating timeline

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -30,14 +30,8 @@ const AboutMe = ({ props, scrollRef }) => {
       <Box maxW={{ base: "100%", lg: "50%" }}>
         <Header description="learn more" title="About Me" />
         <Text mt="10" fontSize="lg" fontWeight={300}>
-          Hello 👋 I'm Sumant, a 21 year old undergraduate student based out of
+          Hello 👋 I'm Sumant, a 24 year old industry professional  based out of
           Seattle, Washington. Welcome to my slice of the web 😀 <br /> <br />
-          I am a junior studying electrical and computer engineering at the
-          University of Washington. Passionate about promoting inclusive
-          education and computational literacy, I work as a head teaching
-          assistant at the Paul G Allen School of Computer Science and
-          Engineering for the introductory computer science series. Teaching has
-          been one of the rewarding part of my college experience yet!
           <br /> <br /> I work on a variety of projects across multiple
           platforms such as: Python, React, Java, ReactNative, JavaScript, HTML,
           CSS and C++. Take a look at some of my projects below to see my work
